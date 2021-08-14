3106e845cf23b43bf2106bd54786e21a977f62e6
VictorBMIAC <86115814+VictorBMIAC@users.noreply.github.com>
2021-08-13 21:32:11 -0600

    Add files via upload

---
+++ HAL/HAL_FSM.c
@@ -0,0 +1,130 @@
+#include "HAL_FSM.h"
+
+// Estados de la FSM
+typedef enum{
+	E0,
+  	E1,
+  	E2,
+  	E3,
+	E4,
+	E5,
+	E6
+}Estados;
+
+// Estado inicial
+Estados EA = E0;
+
+// Funciones por estado
+void EDO_0(int btnU, int btnD){
+	// Codigo de estado inicio
+	rgb("rojo");
+	// Codigo de estado fin
+	// Cambio de estado
+	if(btnU == 1 && btnU != btnUa && btnD == 0)
+		EA = E1;
+	else if(btnD == 1 && btnD != btnDa && btnU == 0)
+		EA = E6;
+	btnUa = btnU;
+	btnDa = btnD;
+}
+
+void EDO_1(int btnU, int btnD){
+	// Codigo de estado inicio
+	rgb("verde");
+	// Codigo de estado fin
+	// Cambio de estado
+	if(btnU == 1 && btnU != btnUa && btnD == 0)
+		EA = E2;
+	else if(btnD == 1 && btnD != btnDa && btnU == 0)
+		EA = E0;
+	btnUa = btnU;
+	btnDa = btnD;
+}
+
+void EDO_2(int btnU, int btnD){
+	// Codigo de estado inicio
+	rgb("azul");
+	// Codigo de estado fin
+	// Cambio de estado
+	if(btnU == 1 && btnU != btnUa && btnD == 0)
+		EA = E3;
+	else if(btnD == 1 && btnD != btnDa && btnU == 0)
+		EA = E1;
+	btnUa = btnU;
+	btnDa = btnD;
+}
+
+void EDO_3(int btnU, int btnD){
+	// Codigo de estado inicio
+	rgb("amarillo");
+	// Codigo de estado fin
+	// Cambio de estado
+	if(btnU == 1 && btnU != btnUa && btnD == 0)
+		EA = E4;
+	else if(btnD == 1 && btnD != btnDa && btnU == 0)
+		EA = E2;
+	btnUa = btnU;
+	btnDa = btnD;
+}
+
+void EDO_4(int btnU, int btnD){
+	// Codigo de estado inicio
+	rgb("celeste");
+	// Codigo de estado fin
+	// Cambio de estado
+	if(btnU == 1 && btnU != btnUa && btnD == 0)
+		EA = E5;
+	else if(btnD == 1 && btnD != btnDa && btnU == 0)
+		EA = E3;
+	btnUa = btnU;
+	btnDa = btnD;
+}
+
+void EDO_5(int btnU, int btnD){
+	// Codigo de estado inicio
+	rgb("morado");
+	// Codigo de estado fin
+	// Cambio de estado
+	if(btnU == 1 && btnU != btnUa && btnD == 0)
+		EA = E6;
+	else if(btnD == 1 && btnD != btnDa && btnU == 0)
+		EA = E4;
+	btnUa = btnU;
+	btnDa = btnD;
+}
+
+void EDO_6(int btnU, int btnD){
+	// Codigo de estado inicio
+	rgb("blanco");
+	// Codigo de estado fin
+	// Cambio de estado
+	if(btnU == 1 && btnU != btnUa && btnD == 0)
+		EA = E0;
+	else if(btnD == 1 && btnD != btnDa && btnU == 0)
+		EA = E5;
+	btnUa = btnU;
+	btnDa = btnD;
+}
+
+// Estructura para asignar una funcion a cada estado
+typedef struct {
+	Estados Estado;
+	void (*func)(int,int);
+} FSM;
+
+// Maquina de estados (asignaciones)
+FSM MDE[] = {
+		{E0,EDO_0},
+        {E1,EDO_1},
+        {E2,EDO_2},
+        {E3,EDO_3},
+		{E4,EDO_4},
+		{E5,EDO_5},
+		{E6,EDO_6}
+};
+
+
+//LL_GPIO_SetOutputPin(GPIOA,LD2_Pin);
+//LL_mDelay(300);
+//LL_GPIO_ResetOutputPin(GPIOA,LD2_Pin);
+//LL_mDelay(300);
---
+++ HAL/HAL_RGB.h
@@ -0,0 +1,25 @@
+#ifndef HAL_RGB_H_
+#define HAL_RGB_H_
+#endif
+void rgb_rojo();
+void rgb_verde();
+void rgb_azul();
+void rgb_amarillo();
+void rgb_celeste();
+void rgb_morado();
+void rgb_blanco();
+void rgb_apagado();
+
+/*
+ * Funcion para encender en un color a RGB
+ * recibe como parametro el color:
+ * Rojo
+ * Verde
+ * Azul
+ * Amarillo
+ * Celeste
+ * Morado
+ * Blanco
+ * Cualquier otro parametro es apagado, pero es
+ * requerido un parametro
+ */
